5c86bd81b185b7d169b402b4846657fe44d9db8c
mluper@protonmail.com <mluper@protonmail.com>
2026-05-18 21:02:40 -0400

    re-learning hooks and passing values

--- warhammer-utilities/src/pages/home/header/header.tsx
+++ warhammer-utilities/src/pages/home/header/header.tsx
@@ -2,17 +2,23 @@
 import * as React from 'react'
 import { Tabs, Tab } from '@mui/material'
 
-export default function Header() {
-    const [selection, setSelection] = React.useState('default');
-    const onSelect = function (_: React.SyntheticEvent, value: string) {
+interface Props {
+    onHeaderSelect: (e: React.SyntheticEvent, value: string) => void
+}
+
+export default function Header(props: Props) {
+    const [selected, setSelection] = React.useState('Test 1');
+
+    const onSelect = (e: React.SyntheticEvent, value: string) => {
         setSelection(value);
+        props.onHeaderSelect(e, value);
     }
 
     return (
-        <Tabs value={selection} onChange={onSelect}>
-            <Tab label='Test 1' />
-            <Tab label='Test 2' />
-            <Tab label='Test 3' />
+        <Tabs value={selected} onChange={onSelect}>
+            <Tab label='Test 1' value='Test 1' />
+            <Tab label='Test 2' value='Test 2' />
+            <Tab label='Test 3' value='Test 3' />
         </Tabs>
     );
 }
--- warhammer-utilities/src/pages/home/home.tsx
+++ warhammer-utilities/src/pages/home/home.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { Box } from '@mui/material'
 import Header from './header/header'
 // footer
@@ -6,15 +7,29 @@ import Header from './header/header'
 /* Functionality
     Searchable data slates
     Dice roller comparing x to y with mods
+
+    mmmk so hooks
+    you can define a value and a setter for it. 
+    yeah this isn't too far from how I'd do it. 
+    define the thing up here and pass it to the child
+    the child either calls it directly or needs to make a wrapper
 */
 
 
 
+
 export default function Home() {
+    const [headerSelection, setHeaderSelection] = React.useState('Test 1');
+
+    const onHeaderSelect = function (_: React.SyntheticEvent, value: string) {
+        setHeaderSelection(value);
+        console.log(value)
+    }
 
     return (
-        <Box>
-            <Header></Header>
-        </Box>
+        <>
+            <Header onHeaderSelect={onHeaderSelect}></Header>
+            <Box id='content'></Box>
+        </>
     )
 }
